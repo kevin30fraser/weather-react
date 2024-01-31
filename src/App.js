6f47769590a602bb906b1aeaf819f245dd239d5f
kevin30fraser <kevin30fraser@gmail.com>
2024-02-01 02:27:00 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [city, setcity] = useState('london');
+  const [deg, setdeg] = useState(0);
+  const [desc, setdesc] = useState('Raining');
+  const [cityvalue, setcityvalue] = useState();
+
+  function handleInput(event) {
+    // console.log(event.target.value)
+    setcityvalue(event.target.value)
+  }
+
+
+  function getdata() {
+    let weather = axios(`https://api.openweathermap.org/data/2.5/weather?q=${cityvalue}&appid=ee949cc184264765ad2f53c854eb1a81`)
+    // console.log(weather);
+
+    weather.then((dalta) => {
+      console.log('Fetched data SuccessFull')
+      console.log(dalta.data);
+      setcity(dalta.data.name);
+      setdeg(dalta.data.main.temp);
+      setdesc(dalta.data.weather[0].description);
+      console.log((dalta.data.name));
+      console.log(dalta.data.main.temp);
+      console.log((dalta.data.weather[0].description));
+    }).catch(()=>{
+      console.log('Failed');
+    })
+
+
+  }
+
+  return (<div className="flex flex-row justify-center h-[100vh] items-center">
+    <div style={{ backgroundImage: "linear-gradient(120deg, #a6c0fe 0%, #f68084 100%)" }} className="px-5 py-2 rounded-md shadow text-center">
+      <h2 className="font-medium">Hey! ⛅</h2>
+      <p className="text-xs md-1">Do you want to know the weather Report :)</p>
+      <input type="text" placeholder="Search" onChange={handleInput}
+        className="rounded-md h-6 text-sm mt-2 mb-1 px-5 py-2 outline-none"></input>
+      <br />
+      <button onClick={getdata} className="bg-black text-white rounded-md px-5 py-2 text-xs mt-2">Get Report</button>
+      <p className="text-xs mt-2">Degree: {deg} | City: {city} | Weather: {desc}</p>
     </div>
-  );
+  </div>);
 }
 
 export default App;
